2a071524dd102c05d731272aad4a84e936b49bc2
dobflob <jess.dobs@gmail.com>
2025-07-23 14:48:15 -0400

    set up main app file with list of TODOs

---
+++ app.py
@@ -0,0 +1,45 @@
+from constants import TEAMS, PLAYERS
+
+# TODO: Clean Data Function
+#   read existing player data
+#   clean player data using copy.deepcopy()
+#   save cleaned data to new collection
+#   data to clean: height (int) and experience (bool)
+#   DO NOT modify existing data in PLAYERS and TEAMS
+
+# TODO: Balance Teams Function
+#   balance players across the 3 teams
+#   teams should have the same number of total players
+
+# TODO: Console Readability
+#   when the menu or stats display to the console, it should be in a nice readable format
+
+# TODO: Displaying Stats
+#   when displaying the selected team's stats, include:
+#   Team's name (str)
+#   Total Players (int)
+#   Player names, separated by commas (strs)
+
+# TODO: clean guardian field
+#   add the guardian field to the data cleaning function: split the guardian string into a list
+#   if more than one guardian, indicated by " and " between the names (split on " and ")
+
+# TODO: Additional Team Balancing
+#   balance team so that each team has the same number of experienced vs inexperienced
+#   if done correctly, each team's stats should display the same count for experienced total and inexperienced total, as well as the same number of players on the team
+
+# TODO: Additional Stats
+#   the number of inexperienced players on the team
+#   the number of experienced players on the team
+#   average height of the team
+#   guardians of all players on the team as a comma separated string
+
+# TODO: Quit menu option
+#   user should be re-prompted with the main menu until they decide to quit the program
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
